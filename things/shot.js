a73dc0e3b13ec3ffbcbd0c8234711a7cd884f14d
olli <oliver.zeigermann@gmail.com>
2014-07-12 15:45:07 +0200

    shots on helicopter target

--- things/shot.js
+++ things/shot.js
@@ -1,10 +1,11 @@
 var SHOT_SPEED = 100;
 
-function Shot(game, x, y, angle) {
+function Shot(game, helicopter, x, y, angle) {
     Phaser.Sprite.call(this, game, x, y, 'shot');
 
     game.physics.enable(this, Phaser.Physics.ARCADE);
 
+    this.helicopter = helicopter;
     this.rotation = angle;
     this.body.velocity.x = Math.cos(angle) * SHOT_SPEED;
     this.body.velocity.y = Math.sin(angle) * SHOT_SPEED;
@@ -13,3 +14,12 @@ function Shot(game, x, y, angle) {
 
 Shot.prototype = Object.create(Phaser.Sprite.prototype);
 Shot.prototype.constructor = Shot;
+
+Shot.prototype.update = function () {
+    this.game.physics.arcade.overlap(this, this.helicopter, this.helicopterHit, null, this);
+};
+
+Shot.prototype.helicopterHit = function (shot, helicopter) {
+    helicopter.kill();
+};
+
--- things/tank.js
+++ things/tank.js
@@ -18,20 +18,16 @@ function Tank(game, helicopter, x, y) {
     this.body.immovable = true;
     this.body.setSize(50, 50);
     this.frame = 2;
-    this.fired = false;
+
+    var timer = game.time.create(false);
+    timer.loop(1000, Tank.prototype.shoot, this);
+    timer.start();
 }
 Tank.prototype = Object.create(Phaser.Sprite.prototype);
 Tank.prototype.constructor = Tank;
 
 Tank.prototype.update = function () {
     this.game.physics.arcade.overlap(this, this.helicopter, this.helicopterHit, null, this);
-
-    var dx = Math.abs(this.position.x - this.helicopter.position.x);
-    var dy = Math.abs(this.position.y - this.helicopter.position.y);
-//    console.log(dx > this.game.camera.width, dx , this.game.camera.width);
-    if (dx < this.game.camera.width / 2) {
-        this.shoot();
-    }
 
 };
 
@@ -40,16 +36,18 @@ Tank.prototype.helicopterHit = function (tank, helicopter) {
 };
 
 Tank.prototype.shoot = function () {
-    if (!this.fired) {
-        var dx = this.position.x - this.helicopter.position.x;
-        var dy = this.position.y - this.helicopter.position.y;
 
-        var degree = Math.tan(dy/dx);
-
-        var shot = new Shot(game, this.position.x, this.position.y - 10, degree);
-        game.add.existing(shot);
-
-        this.fired = true;
+    var dx = Math.abs(this.position.x - this.helicopter.position.x);
+    var dy = Math.abs(this.position.y - this.helicopter.position.y);
+//    console.log(dx > this.game.camera.width, dx , this.game.camera.width);
+    if (dx > this.game.camera.width / 2) {
+        return;
     }
 
+    var turretY = this.position.y - 10;
+
+    var degree = game.physics.arcade.angleBetween(this, this.helicopter);
+
+    var shot = new Shot(game, this.helicopter, this.position.x, turretY, degree);
+    game.add.existing(shot);
 };
